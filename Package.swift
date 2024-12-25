dbe58b7ad916c4ff4c5ec15455328412aae342cf
bahielfeky <bahi.elfeky@gmail.com>
2024-12-25 15:17:29 +0200

    updating package file

--- Package.swift
+++ Package.swift
@@ -17,8 +17,8 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
+        .binaryTarget(name: "EnrollFramework", path: "artifacts/EnrollFramework.xcframework"),
         .target(
-            name: "EnrollPackage"),
-        .binaryTarget(name: "EnrollFramework", path: "artifacts/EnrollFramework.xcframework")
+            name: "EnrollPackage", dependencies: ["EnrollFramework", .product(name: "DotDocument", package: "dot-ios-sdk-spm"), .product(name: "DotFaceVerification", package: "dot-ios-sdk-spm")]),
     ]
 )
